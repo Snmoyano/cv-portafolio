a15217b1512bf3ac93bdf762737e96b430ef8641
Snmoyano <sergio.nicolas.moyano@gmail.com>
2022-11-03 21:36:57 -0300

    update

--- src/Form.jsx
+++ src/Form.jsx
@@ -3,22 +3,27 @@ import "./styles/form.css";
 
 const Form = () => {
   return (
-    <form name="contact" method="post">
+    <form className="container__form" name="contact" method="post">
+      <h3>Contactame</h3>
       <input type="hidden" name="form-name" value="contact" />
-      <p>
+      <p className="form__div">
         <label htmlFor="name">Name</label> <br />
         <input type="text" id="name" name="name" required />
       </p>
-      <p>
+      <p className="form__div">
         <label htmlFor="email">Email</label> <br />
         <input type="email" id="email" name="email" required />
       </p>
-      <p>
+      <p className="form__div">
         <label htmlFor="message">Message</label> <br />
         <textarea id="message" name="message" required></textarea>
       </p>
       <p>
-        <input type="submit" value="Submit message" />
+        <input
+          className="flowrino-btn btn"
+          type="submit"
+          value="Submit message"
+        />
       </p>
     </form>
   );
